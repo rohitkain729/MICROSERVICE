edeeb6a53cda94dd0d2425289b65f2b3a24d1061
rohitkain729 <rohit729verma@gmail.com>
2025-11-06 01:22:57 +0530

    3 commit billing service

---
+++ 3-microservice-proj-134-26rdjuly-col2-Billing-Service/src/main/java/com/app/ms/BillingServiceRestController.java
@@ -0,0 +1,29 @@
+package com.app.ms;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/billing-api")
+public class BillingServiceRestController {
+
+	@Value("${server.port}")
+	private int port;
+
+	@Value("${eureka.instance.instance-id}")
+	private String instance_id;
+
+	@GetMapping("/payment")
+	public ResponseEntity<String> doPayment() {
+
+		
+		return new ResponseEntity<String>("payment can be done using Cards:::" + instance_id + "----" + port,
+				HttpStatus.OK);
+
+	}
+
+}
